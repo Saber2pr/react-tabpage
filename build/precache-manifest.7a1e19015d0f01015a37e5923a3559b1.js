29f320ef3f95b041d75fc06683bf491c6b9f618c
saber2pr <saber2pr@gmail.com>
2018-10-14 20:46:41 +0800

    addBuild

---
+++ build/precache-manifest.7a1e19015d0f01015a37e5923a3559b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07945ed723184ee3146ff31d64b4a003",
+    "url": "./static/media/MarkerFelt.07945ed7.ttf"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f917388743293adf26d6",
+    "url": "./static/js/main.f9173887.chunk.js"
+  },
+  {
+    "revision": "13a15114ae3ac597de25",
+    "url": "./static/js/1.13a15114.chunk.js"
+  },
+  {
+    "revision": "f917388743293adf26d6",
+    "url": "./static/css/main.8dc07328.chunk.css"
+  },
+  {
+    "revision": "3aa2e479059ced94c6efe2207db1e794",
+    "url": "./index.html"
+  }
+];
